a16e90cefb75f2738d4c4f20a8a5e45d1e79ef0b
Jack <jkigh23@wgu.edu>
2024-08-27 20:26:10 -0400

    Updated model for alpaca auth token

--- Backend/djangoBackend/api/models.py
+++ Backend/djangoBackend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class Stock(models.Model):
     symbol = models.CharField(max_length=10)
@@ -17,3 +18,11 @@ class TopStock(models.Model):
 
     def __str__(self):
         return f'Top Stock: {self.symbol} on {self.date}'
+    
+class UserToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='alpaca_token')
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Alpaca Token"

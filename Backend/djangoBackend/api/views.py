f1f8ee0eeed292e2f4743a0d5e5c7a321da979a1
Jack <jkigh23@wgu.edu>
2024-09-03 10:50:17 -0400

    Added encoded jwt

--- Backend/djangoBackend/api/views.py
+++ Backend/djangoBackend/api/views.py
@@ -17,6 +17,7 @@ from .custom_auth import InternalAPIAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
 from .custom_permissions import IsAuthenticatedOrInternal
 from datetime import datetime
+from urllib.parse import quote
 
 
 load_dotenv()
@@ -143,7 +144,8 @@ def callback_view(request):
         except Exception as e:
             return JsonResponse({'error': f'Error Updating Token: {str(e)}'}, status=500)
 
-        redirect_url = 'https://wsb-bot-5lj7.vercel.app/connected?jwt={jwt_token}'
+        encoded_jwt = quote(jwt_token)
+        redirect_url = f'https://wsb-bot-5lj7.vercel.app/connected?jwt={encoded_jwt}'
         return HttpResponseRedirect(redirect_url)
     else:
         error_message = f'Failed to obtain access token. Response: {response.text}'
